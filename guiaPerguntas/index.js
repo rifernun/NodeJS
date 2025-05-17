0ab0b6592fa7011cb8d8e0fbf58c411abbb9aa62
Richard PC <rifernun@gmail.com>
2025-05-17 11:20:42 -0300

    edição dos index.js/ejs

--- guiaPerguntas/index.js
+++ guiaPerguntas/index.js
@@ -7,13 +7,24 @@ app.set('view engine', 'ejs');
 app.get("/:nome/:lang", function(req,res){
     var nome = req.params.nome;
     var lang = req.params.lang;
-    let exibirmsg = false;
+    var exibirmsg = false;
+
+    var produtos = [
+        {nome:"doritos", preco: 3.14},
+        {nome:"coca-cola", preco: 5.00},
+        {nome:"leite", preco: 3.45},
+        {nome:"carne", preco: 25.50},
+        {nome:"redbull", preco: 15.00},
+        {nome:"nescau", preco: 10.00}
+    ];
+
     res.render('index', { //render, renderiza objetos dentro de onde for definido na views
         nome: nome,
         lang: lang,
         empresa: "youtube",
         empregados: 5000,
-        msg: exibirmsg
+        msg: exibirmsg,
+        produtos: produtos
     }); //desenhando em html o index do meu view
 });
 
